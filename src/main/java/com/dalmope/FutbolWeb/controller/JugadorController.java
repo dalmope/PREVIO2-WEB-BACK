ac6db8c39a930fe8c350ff4b903ea3059b808e8a
dalmope <eukadaju@gmail.com>
2021-11-25 21:35:58 -0500

    Jugador controller full

--- src/main/java/com/dalmope/FutbolWeb/controller/JugadorController.java
+++ src/main/java/com/dalmope/FutbolWeb/controller/JugadorController.java
@@ -10,8 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,5 +50,37 @@ public class JugadorController {
     public Jugador getJugador(@PathVariable("id") Integer id) {
         return jugadorService.findById(id);
     }
+
+    @ApiOperation("Crea un Jugador")
+    @PostMapping
+    public ResponseEntity<?> createJugador(@RequestBody Jugador jugador) {
+        if (jugadorService.findById(jugador.getId()) != null) {
+            return new ResponseEntity<>(new Mensaje("ya existe"), HttpStatus.CONFLICT);
+        }
+        jugadorService.save(jugador);
+        return new ResponseEntity<>(new Mensaje("creado"), HttpStatus.CREATED);
+    }
+
+    @ApiOperation("Actualiza un Jugador")
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateJugador(@PathVariable("id") Integer id, @RequestBody Jugador jugador) {
+        if (jugadorService.findById(id) == null) {
+            return new ResponseEntity<>(new Mensaje("no existe"), HttpStatus.NOT_FOUND);
+        }
+        jugador.setId(id);
+        jugadorService.save(jugador);
+        return new ResponseEntity<>(new Mensaje("actualizado"), HttpStatus.OK);
+    }
+    
+    @ApiOperation("Borra un Jugador por su ID")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteJugador(@PathVariable("id") Integer id) {
+        if (jugadorService.findById(id) == null) {
+            return new ResponseEntity<>(new Mensaje("no existe"), HttpStatus.NOT_FOUND);
+        }
+        jugadorService.delete(id);
+        return new ResponseEntity<>(new Mensaje("eliminado"), HttpStatus.OK);
+    }
+    
 
 }
